9bbba812f938be88dcaac01f8f7f75d8a982a381
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2026-04-21 22:03:54 +0100

    fix port for railway

--- server/server/index.js
+++ server/server/index.js
@@ -148,6 +148,7 @@ app.use(express.static("server"));
 
 // 🚀 Start server
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on port", PORT);
 });
