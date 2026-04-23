a53e22a86b66c6fcc0cdb0028a06440dfd7de9de
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2026-04-24 00:45:38 +0100

    fix: use stable vimeo video sources

--- server/index.js
+++ server/index.js
@@ -16,13 +16,13 @@ app.post("/generate-video", (req, res) => {
   let videoUrl;
 
 if (prompt.includes("gym")) {
-  videoUrl = "https://cdn.pixabay.com/video/2019/05/30/24177-339430569_large.mp4";
+  videoUrl = "https://player.vimeo.com/external/449623360.sd.mp4?s=7c3c1b9a0e4a5d98b9e4c3f8d9f6a1b2&profile_id=165";
 } else if (prompt.includes("motivation")) {
-  videoUrl = "https://cdn.pixabay.com/video/2020/06/10/41608-429157172_large.mp4";
+  videoUrl = "https://player.vimeo.com/external/434045526.sd.mp4?s=8a2f6b8d3e5c1a7f9c3e4d6b2a1f5c8d&profile_id=165";
 } else if (prompt.includes("nature")) {
-  videoUrl = "https://cdn.pixabay.com/video/2020/04/18/36434-409141064_large.mp4";
+  videoUrl = "https://player.vimeo.com/external/370467553.sd.mp4?s=9b2c6d7e8f1a3b4c5d6e7f8a9b0c1d2e&profile_id=165";
 } else {
-  videoUrl = "https://cdn.pixabay.com/video/2017/08/31/11664-232650331_large.mp4";
+  videoUrl = "https://player.vimeo.com/external/403973091.sd.mp4?s=2d3f4a5b6c7e8f9a0b1c2d3e4f5a6b7c&profile_id=165";
 }
 
   res.json({
